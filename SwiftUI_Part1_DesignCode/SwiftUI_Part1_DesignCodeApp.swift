84970308b93e260279a6f577e66adad230c3ea91
Simon McNeil <simonmcneil062@gmail.com>
2021-12-30 09:05:31 -0500

    Initial Commit

---
+++ SwiftUI_Part1_DesignCode/SwiftUI_Part1_DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Part1_DesignCodeApp.swift
+//  SwiftUI_Part1_DesignCode
+//
+//  Created by Simon Mcneil on 2021-12-30.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Part1_DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
